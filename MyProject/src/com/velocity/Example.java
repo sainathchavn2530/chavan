04814338707b3e42e4333fd88ee33ab6822cef11
Your Name <you@example.com>
2023-01-08 22:19:46 +0530

    Add arrry list

---
+++ MyProject/src/com/velocity/Example.java
@@ -0,0 +1,18 @@
+package com.velocity;
+
+import java.util.ArrayList;
+
+public class Example {
+
+	public static void main(String[] args) {
+	
+		ArrayList<Integer> al = new ArrayList<Integer>();
+		al.add(10);
+		al.add(20);
+		al.add(30);
+		al.add(40);
+		System.out.println(al);
+
+	}
+
+}
