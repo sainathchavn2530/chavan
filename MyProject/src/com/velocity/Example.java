b369fe318b6f9de2c5004c239e9ec04d3ff0b0a6
Your Name <you@example.com>
2023-01-09 11:02:47 +0530

    second version added in AL

--- MyProject/src/com/velocity/Example.java
+++ MyProject/src/com/velocity/Example.java
@@ -9,8 +9,8 @@ public class Example {
 		ArrayList<Integer> al = new ArrayList<Integer>();
 		al.add(10);
 		al.add(20);
-		al.add(30);
-		al.add(40);
+		al.add(50);
+		al.add(60);
 		System.out.println(al);
 
 	}
